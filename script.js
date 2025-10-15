1e76d14bb0c3348f6f0ac0ce7b7006a0872eef93
Javier <Javier@gmail.com>
2025-10-15 16:55:29 +0200

    arreglar img bacground

--- script.js
+++ script.js
@@ -1,79 +1,69 @@
 function main() {
-    //const geo = navigator.geolocation
-    //const a = geo.getCurrentPosition(success, error)
-
+    onPosition()
     searchBarre()
-    
+
 }
 
+function onPosition() {
+    const geo = navigator.geolocation
+    const a = geo.getCurrentPosition(success, error)
+}
 
 function success(position) {
-    //console.log(position)
-    let latitude = position.coords.latitude;
-    //console.log(latitude)
-    let longitude = position.coords.longitude;
-    //console.log(longitude)
 
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
 
     getweather({
         latitude: latitude,
         longitude: longitude
     });
-    
 }
 
 function error() {
     console.log("Unable to retrieve your location")
 }
 
-
-
 async function getCity(position_obj) {
-    
-const lat = position_obj["latitude"]
-const long = position_obj["longitude"]
-const coord = await fetch(` https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json&accept-language=fr;`)  
-const city = await coord.json()
-const cityName = city.address.city
-settitle(cityName)
 
-
-
-}
-async function getweather(position_obj) {
-
-   // console.log("entro en weather")
-   
     const lat = position_obj["latitude"]
     const long = position_obj["longitude"]
-        getCity({
-     latitude: lat,
-  longitude: long
-})
- 
-    
+    const coord = await fetch(` https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json&accept-language=fr;`)
+    const city = await coord.json()
+    const cityName = city.address.city
+    const countryName = city.address.country
+    settitle(cityName, countryName)
+
+}
+
+async function getweather(position_obj) {
+
+    const lat = position_obj["latitude"]
+    const long = position_obj["longitude"]
+    getCity({
+        latitude: lat,
+        longitude: long
+    })
 
     try {
 
-        const clima = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m&current=temperature_2m&timezone=auto`)
-
+        const clima = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=is_day,temperature_2m,weather_code&timezone=auto`)
         const clima_obj = await clima.json()
 
-        console.log(clima_obj)
+        console.table(clima_obj)
+
+        console.log(clima_obj.current.is_day)
+        console.log(clima_obj.current.weather_code)
+        console.log(clima_obj.current.temperature_2m)
 
         const temperature = clima_obj.current.temperature_2m;
-        console.log(temperature);
+
         setTemperature(temperature)
-
-
-
+        setImgMeteo(clima_obj.current.weather_code)
 
     } catch (error) {
-        console.log("no tengo el clima")
+        console.error("no tengo el clima")
     }
-
-
-
 
 }
 
@@ -81,43 +71,15 @@ async function searchBarre() {
 
     const searchItem = document.querySelector(".searchItem")
 
-    //console.log(searchItem)
-
     searchItem.addEventListener("submit", function search(event_obj) {
 
         event_obj.preventDefault()
 
         const formData_obj = new FormData(searchItem)
-        //console.log(formData_obj);
         const city = formData_obj.get("city")
-        //console.log(city)
-        //const latitude = formData_obj.get("latitude")
-        //console.log(latitude)
-        //const longitude = formData_obj.get("longitude")
-        //console.log(longitude)
+        setCoordenade(city.trim())
 
-        //const longitudeValue = longitude.trim()
-        //const latitudValue = latitude.trim();
-        //const decimalRegex = /^-?\d*\.?\d+$/;
-
-        if (city.trim() == "") {
-
-            /*if (decimalRegex.test(latitudValue)&&decimalRegex.test(longitudeValue)) {
-                
-                console.log('Número decimal válido:', latitudValue, longitudeValue);
-            } else {
-                
-                console.log("no sirve")
-            }*/
-        } else {
-
-            console.log("pasa bien")
-            setCoordenade(city.trim())
-
-
-        }
-
-
+      
     })
 
 }
@@ -127,11 +89,6 @@ async function setCoordenade(city) {
     try {
         const elementCity = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=" + city + "&count=1&language=en&format=json")
         const coorde_obj = await elementCity.json()
-
-        //console.log(coorde_obj)
-        // console.log(coorde_obj["results"])
-        //console.log(coorde_obj["results"][0]["latitude"])
-        //console.log(coorde_obj["results"][0]["longitude"])
         const latitud = coorde_obj["results"][0]["latitude"]
         const longitud = coorde_obj["results"][0]["longitude"]
         settitle(city)
@@ -145,33 +102,54 @@ async function setCoordenade(city) {
     }
 }
 
-function settitle(city) {
+function settitle(city, country) {
 
-    const titleElement = document.querySelector(".data-front")
-    //titleElement.textContent = ""
-    const title = document.createElement("h1")
-    title.textContent = city
-    titleElement.appendChild(title)
+    const titleElement = document.querySelector(".city-title").textContent = city
+     const subTitleElement = document.querySelector(".country-title").textContent = country
 }
 
-function setImgMeteo() {
+function setImgMeteo(weather_code) {
+
+    console.log("entro en la funcion img")
+
+    if(!weather_code <= 48){
+
+        console.log("entro en el 1 if")   
+        if(!weather_code >= 51 || weather_code <= 57 ){
+            console.log("2 if")
+            if(!weather_code >=61 || weather_code <= 67){
+                console.log("3 if")
+                if(){}                }
+                document.body.classList.remove
+                document.body.classList.toggle("snow")
+    
+
+            }else{
+                document.body.classList.remove
+                document.body.classList.toggle("storn")
+    
+            }
+        }else{
+
+            document.body.classList.toggle("raini")
+    
+        }
+    }else{
+
+        return
+    
+    }
+
+
+    
+    
 
 }
 
 function setTemperature(temperature) {
-    
 
-    const titleElement = document.querySelector(".data-front")
-    const temper = document.createElement("p")
-    temper.textContent = temperature + "°"
-    titleElement.appendChild(temper)
+    const titleElement = document.querySelector(".temperature").textContent = temperature + "°"
+
 }
 
-
-
-
-
-//https://api.open-meteo.com/v1/forecast?latitude=43.6043&longitude=1.4437&hourly=temperature_2m
-//"https://api.open-meteo.com/v1/forecast" + latitude&longitude + "&hourly=temperature_2m"*/
-//https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json
 main();
